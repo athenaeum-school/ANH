e62af33ee6a84ea5e16b8bbd483a32bd0f8ef4a1
headrokoko <supermerio1008@hotmail.co.jp>
2014-12-03 20:51:06 +0900

    20141203 モンスター図鑑を作成 仮のメニューシーンと図鑑シーンの切替と各モンスターの説明シーンを作成 図鑑シーンでF1~F6を押すことでモンスターの表示をアンロック

---
+++ Assets/Code/States/Pet6Explanation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Code.Interfaces;
+
+namespace Assets.Code.States{
+	public class Pet6Explanation : ISceneState {
+		
+		private BookStateManager bookmanager;
+		
+		public Pet6Explanation(BookStateManager Bmanager){
+			bookmanager = Bmanager;
+			Time.timeScale = 0;
+		}
+		
+		public void StateUpdate(){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				Debug.Log("図鑑画面に遷移");
+				bookmanager.StateChange(new BookState(bookmanager));
+			}
+		}
+		
+		public void Render(){
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),
+			                bookmanager.pet6texExplanation,
+			                ScaleMode.StretchToFill);
+			if(GUI.Button(new Rect((Screen.width/10)*8,(Screen.height/10)*9,
+			                       (Screen.width/10)*2,(Screen.height/10)*1),
+			              "一覧に戻る")){
+				Time.timeScale = 0;
+				Debug.Log("図鑑画面に遷移");
+				bookmanager.StateChange(new BookState(bookmanager));
+			}
+		}
+	}
+}
