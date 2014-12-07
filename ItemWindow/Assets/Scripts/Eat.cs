578e1ff7473a4882def2e109905126f7b1ce7f1f
DaikiNakajima3564 <hitanykey.del@gmail.com>
2014-12-08 01:22:50 +0900

    ItemWindow_ドロップ時の反応を確認 ・今後の食事中～後の処理はここに記述

---
+++ ItemWindow/Assets/Scripts/Eat.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eat : DragDropSurface {
+
+
+	protected override void eat(){
+		//アイテムドロップ(食事)中～後の処理をここに記述予定
+		Debug.Log ("eat2");
+	}
+
+}
