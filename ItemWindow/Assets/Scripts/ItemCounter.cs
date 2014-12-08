98e3dacd6091184532f1ba138e80e9820b75eca6
DaikiNakajima3564 <hitanykey.del@gmail.com>
2014-12-08 14:25:37 +0900

    ItemWindow_クリックでアイテムの使用数が増えるように更新 ・まとめて増やす方法を模索中

---
+++ ItemWindow/Assets/Scripts/ItemCounter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemCounter : MonoBehaviour
+{
+	private UILabel label;
+	private int count = 0;
+
+	void Start ()
+	{
+		GetLabelComponent ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	public void ItemIncrement()
+	{	
+		++count;
+		for(int i = 0; i < count; i++) 
+		label.text = i.ToString () + "/99";
+	}
+
+	/*public void ItemIncrementX(){
+		count += 10;
+		for(int i = 0; i < count; i++) 
+			label.text = i.ToString () + "/99";
+	}*/
+
+	public void GetLabelComponent(){
+		label = GameObject.Find("Label_Count").GetComponent<UILabel>();
+	}
+
+
+
+}
